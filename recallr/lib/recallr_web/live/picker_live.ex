e073e992d6cf5392c890ad7994173d5103682db9
Mahir <my@foundea.com>
2021-03-25 20:22:56 +0100

    library live stuff

--- recallr/lib/recallr_web/live/picker_live.ex
+++ recallr/lib/recallr_web/live/picker_live.ex
@@ -1,18 +1,19 @@
 defmodule RecallrWeb.PickerLive do
   use RecallrWeb, :live_view
-  alias Recallr.Prose
+  # alias Recallr.Prose
+  alias Recallr.Library
 
  def mount(_params, _session, socket) do
-  {:ok, socket |> assign(id: Prose.first) |> lookup()}
+  {:ok, socket |> assign(id: Library.first().id) |> lookup()}
  end
 
  def lookup(socket) do
-   prose = Prose.find(socket.assigns.id)
+   prose = Library.get_passage!(socket.assigns.id)
    assign(socket, prose: prose)
  end
 
  def next(socket) do
-  socket |> update(:id, &Prose.next/1) |> lookup
+  socket |> update(:id, &Library.next/1) |> lookup
  end
 
  def handle_event("next", _value, socket) do
